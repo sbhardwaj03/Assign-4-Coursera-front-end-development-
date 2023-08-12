8ee6d1849a9af470bcbbeb473371c40269bf1af4
sbhardwaj03 <134839853+sbhardwaj03@users.noreply.github.com>
2023-08-12 13:48:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -30,3 +30,18 @@
   // STEP 11: Expose the 'byeSpeaker' object to the global scope
   window.byeSpeaker = byeSpeaker;
 })();
+
+(function () {
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+  for (var i = 0; i < names.length; i++) {
+    var firstLetter = names[i].charAt(0).toLowerCase();
+
+    if (firstLetter === 'j') {
+      byeSpeaker.speak(names[i]);
+    } else {
+      helloSpeaker.speak(names[i]);
+    }
+  }
+})();
+
